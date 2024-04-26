8d43abdbfeb0984c7474f3f84088e0a1c411eca2
Law Chu Ming / Ben Law <chumaolaw@gmail.com>
2024-04-26 12:30:49 +0800

    Update fetch_sermons.jsx

--- src/components/fetch_sermons.jsx
+++ src/components/fetch_sermons.jsx
@@ -4,7 +4,7 @@ import $ from "jquery";
 export function fetchSermonData(getQuery, k) {
     $.ajax({
         type: "POST",
-        url: "/search",
+        url: "https://sermon-search-api.onrender.com/search",
         headers: {
             "Content-type": "application/json; charset=UTF-8",
             "Access-Control-Allow-Origin": "*",
